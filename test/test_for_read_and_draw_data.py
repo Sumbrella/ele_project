c2adde4f7b7a974999fd4357ff7bc8aa1c1fa4ea
Sumbrella Wang <493972246@qq.com>
2020-11-03 13:56:28 +0800

    测试绘制功能

---
+++ test/test_for_read_and_draw_data.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+from common.unit import SingleFile
+
+before_filepath = '../data/origin/before/LINE_100_dbdt.dat'
+after_filepath = '../data/origin/after/new_LINE_100_dbdt.dat'
+
+
+before_file = SingleFile(before_filepath)
+after_file = SingleFile(after_filepath)
+
+# read the first point
+before_point = before_file.get_one_point()
+after_point = after_file.get_one_point()
+
+before_point.plot(show=False)
+after_point.plot(show=False)
+
+print(
+f"""
+after_point_size: {after_point._size}
+before_point_size: {before_point._size}
+"""
+)
+print(after_point.get_data())
+
+plt.show()
