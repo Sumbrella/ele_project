fef6860f89c03a753876e2ee65d82b4c4fc608ea
Sumbrella Wang <493972246@qq.com>
2020-11-05 09:49:59 +0800

    搭建Draknet 53网络结构

--- solutions/polyfit/main.py
+++ solutions/polyfit/main.py
@@ -1,40 +1,205 @@
 from paddle import fluid
-from paddle.fluid.dygraph.nn import Linear, Dropout
+from paddle.fluid.dygraph.nn import Conv2D, ParamAttr
+from paddle.fluid.dygraph import BatchNorm, to_variable
+from paddle.fluid.regularizer import L2Decay
 
 
-class Network(fluid.dygraph.Layer):
+class ConvBNLayer(fluid.dygraph.Layer):
+    """
+    卷积 + 批归一化，BN层之后激活函数默认用leaky_relu
+    """
+    def __init__(self,
+                 ch_in,
+                 ch_out,
+                 filter_size=(1, 3),
+                 stride=1,
+                 groups=1,
+                 padding=0,
+                 act="leaky",
+                 is_test=True):
+        super(ConvBNLayer, self).__init__()
 
-    def __init__(self, input_dim, out_dim=9):
-        super(Network, self).__init__()
+        self.conv = Conv2D(
+            num_channels=ch_in,
+            num_filters=ch_out,
+            filter_size=filter_size,
+            stride=stride,
+            padding=padding,
+            groups=groups,
+            param_attr=ParamAttr(
+                initializer=fluid.initializer.Normal(0., 0.02)),
+            bias_attr=False,
+            act=None)
 
-        self.fc1 = Linear(
-            input_dim=input_dim,
-            output_dim=50,
-            param_attr=fluid.param_attr.ParamAttr(initializer=fluid.initializer.Normal(0, 0.02)),
-            bias_attr=fluid.param_attr.ParamAttr(initializer=fluid.initializer.Normal(0, 0.03)),
-            act='relu'
-        )
+        self.batch_norm = BatchNorm(
+            num_channels=ch_out,
+            is_test=is_test,
+            param_attr=ParamAttr(
+                initializer=fluid.initializer.Normal(0., 0.02),
+                regularizer=L2Decay(0.)),
+            bias_attr=ParamAttr(
+                initializer=fluid.initializer.Constant(0.0),
+                regularizer=L2Decay(0.)))
 
-        self.fc2 = Linear(
-            input_dim=50,
-            output_dim=out_dim,
-            param_attr=fluid.param_attr.ParamAttr(initializer=fluid.initializer.Normal(0, 0.02)),
-            bias_attr=fluid.param_attr.ParamAttr(initializer=fluid.initializer.Normal(0, 0.03)),
-            act='relu'
-        )
+        self.act = act
 
-        self.dropout1 = Dropout(p=0.5)
-        self.dropout2 = Dropout(p=0.5)
+    def forward(self, inputs):
+        out = self.conv(inputs)
+        out = self.batch_norm(out)
+        if self.act == 'leaky':
+            out = fluid.layers.leaky_relu(x=out, alpha=0.1)
+        return out
 
-    def forward(self, input):
-        output = self.fc1(input)
-        output = self.dropout1(output)
-        output = self.fc2(output)
 
-        return output
+
+class DownSample(fluid.dygraph.Layer):
+    """
+    下采样，图片尺寸减半，具体实现方式是使用stirde=2的卷积
+    """
+    def __init__(self,
+                 ch_in,
+                 ch_out,
+                 filter_size=(1, 3),
+                 stride=2,
+                 padding=1,
+                 is_test=True):
+
+        super(DownSample, self).__init__()
+
+        self.conv_bn_layer = ConvBNLayer(
+            ch_in=ch_in,
+            ch_out=ch_out,
+            filter_size=filter_size,
+            stride=stride,
+            padding=padding,
+            is_test=is_test)
+        self.ch_out = ch_out
+
+    def forward(self, inputs):
+        out = self.conv_bn_layer(inputs)
+        return out
+
+
+class BasicBlock(fluid.dygraph.Layer):
+    """
+    基本残差块的定义，输入x经过两层卷积，然后接第二层卷积的输出和输入x相加
+    """
+    def __init__(self, ch_in, ch_out, is_test=True):
+        super(BasicBlock, self).__init__()
+
+        self.conv1 = ConvBNLayer(
+            ch_in=ch_in,
+            ch_out=ch_out,
+            filter_size=1,
+            stride=1,
+            padding=0,
+            is_test=is_test
+            )
+        self.conv2 = ConvBNLayer(
+            ch_in=ch_out,
+            ch_out=ch_out*2,
+            filter_size=3,
+            stride=1,
+            padding=1,
+            is_test=is_test
+            )
+
+    def forward(self, inputs):
+        conv1 = self.conv1(inputs)
+        conv2 = self.conv2(conv1)
+        out = fluid.layers.elementwise_add(x=inputs, y=conv2, act=None)
+        return out
+
+
+class LayerWarp(fluid.dygraph.Layer):
+    """
+    添加多层残差块，组成Darknet53网络的一个层级
+    """
+    def __init__(self, ch_in, ch_out, count, is_test=True):
+        super(LayerWarp,self).__init__()
+
+        self.basicblock0 = BasicBlock(ch_in,
+            ch_out,
+            is_test=is_test)
+        self.res_out_list = []
+        for i in range(1, count):
+            res_out = self.add_sublayer("basic_block_%d" % (i),
+                BasicBlock(ch_out*2,
+                    ch_out,
+                    is_test=is_test))
+            self.res_out_list.append(res_out)
+
+    def forward(self,inputs):
+        y = self.basicblock0(inputs)
+        for basic_block_i in self.res_out_list:
+            y = basic_block_i(y)
+        return y
+
+
+DarkNet_cfg = {53: ([1, 2, 8, 8, 4])}
+
+
+class DarkNet53_conv_body(fluid.dygraph.Layer):
+    def __init__(self,
+
+                 is_test=True):
+        super(DarkNet53_conv_body, self).__init__()
+        self.stages = DarkNet_cfg[53]
+        self.stages = self.stages[0:5]
+
+        # 第一层卷积
+        self.conv0 = ConvBNLayer(
+            ch_in=2,
+            ch_out=32,
+            filter_size=(1, 3),
+            stride=1,
+            padding=1,
+            is_test=is_test)
+
+        # 下采样，使用stride=2的卷积来实现
+        self.downsample0 = DownSample(
+            ch_in=32,
+            ch_out=32 * 2,
+            is_test=is_test)
+
+        # 添加各个层级的实现
+        self.darknet53_conv_block_list = []
+        self.downsample_list = []
+        for i, stage in enumerate(self.stages):
+            conv_block = self.add_sublayer(
+                "stage_%d" % (i),
+                LayerWarp(32 * (2 ** (i + 1)),
+                          32 * (2 ** i),
+                          stage,
+                          is_test=is_test))
+            self.darknet53_conv_block_list.append(conv_block)
+        # 两个层级之间使用DownSample将尺寸减半
+        for i in range(len(self.stages) - 1):
+            downsample = self.add_sublayer(
+                "stage_%d_downsample" % i,
+                DownSample(ch_in=32 * (2 ** (i + 1)),
+                           ch_out=32 * (2 ** (i + 2)),
+                           is_test=is_test))
+            self.downsample_list.append(downsample)
+
+    def forward(self, inputs):
+        out = self.conv0(inputs)
+        # print("conv1:",out.numpy())
+        out = self.downsample0(out)
+        # print("dy:",out.numpy())
+        blocks = []
+        for i, conv_block_i in enumerate(self.darknet53_conv_block_list):  # 依次将各个层级作用在输入上面
+            out = conv_block_i(out)
+            blocks.append(out)
+            if i < len(self.stages) - 1:
+                out = self.downsample_list[i](out)
+
+        return blocks[-1:-4:-1]  # 将C0, C1, C2作为返回值
+
 
 
 if __name__ == '__main__':
+    import numpy as np
     from common.unit import SingleFile
 
     before_data_path = "../../data/origin/before/LINE_100_dbdt.dat"
@@ -44,7 +209,14 @@ if __name__ == '__main__':
     after_data_path = SingleFile(after_data_path)
 
     with fluid.dygraph.guard():
-         point = before_file.get_one_point()
+        backbone = DarkNet53_conv_body(is_test=False)
+        # x = np.random.randn(1, 2, 1, 640).astype('float32')
+        point = before_file.get_one_point()
+        x = point.get_narray().reshape(1, 2, 1, 100)
+        x = x.astype('float32')
+        x = to_variable(x)
+        C0, C1, C2 = backbone(x)
+        print(C0.shape, C1.shape, C2.shape)
 
 
 
