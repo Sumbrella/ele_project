7b268b2d09130809c0b2b3a25f95e7981f3d5f43
Sumbrella Wang <493972246@qq.com>
2020-12-19 01:03:11 +0800

    测试自动生成的数据

--- test/test_for_read_and_draw_data.py
+++ test/test_for_read_and_draw_data.py
@@ -4,8 +4,8 @@ from ele_common.units import SingleFile
 # before_filepath = '../data/origin/before/LINE_100_dbdt.dat'
 # after_filepath = '../data/origin/after/new_LINE_100_dbdt.dat'
 
-before_filepath = "../data/generate/data/LINE_001_dbdt.dat"
-after_filepath = "../data/generate/teacher/NEW_LINE_001_dbdt.dat"
+before_filepath = "../data/generate/data/LINE_0001_dbdt.dat"
+after_filepath = "../data/generate/teacher/NEW_LINE_0001_dbdt.dat"
 
 before_file = SingleFile(before_filepath)
 after_file = SingleFile(after_filepath)
@@ -14,8 +14,9 @@ after_file = SingleFile(after_filepath)
 before_point = before_file.get_one_point()
 after_point = after_file.get_one_point()
 
-before_point.plot(show=False)
-after_point.plot(show=False)
+before_point.plot(show=False, alpha=0.5)
+after_point.plot(show=False, alpha=0.5)
+
 
 print(
 f"""
