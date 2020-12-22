6cce3f2197b912e62ece4458460a043654b87737
Sumbrella Wang <493972246@qq.com>
2020-12-22 14:25:37 +0800

    增加generator接口

--- ele_common/units/generator.py
+++ ele_common/units/generator.py
@@ -9,24 +9,25 @@ from solutions.lstm_with_cnn.config import *
 
 class Generator:
     def __init__(self,
-                 layer_number=None,
+                 layer_number_scope=None,
                  depth_scope=None,
                  resistant_scope=None,
                  time_sequence_number=None,
                  time_scope=None,
-                 perturbation_scope=None
+                 square_scope=None,
+                 perturbation_rate=None,
                  ):
-        self.layer_number = layer_number
-        self.depth_scope = depth_scope
-        self.resistant_scope = resistant_scope
-        self.time_scope = time_scope
-        self.time_sequence_number = time_sequence_number
-        self.perturbation_scope = perturbation_scope
+        self.layer_number_scope = layer_number_scope or default_layer_scope
+        self.depth_scope = depth_scope or default_deep_scope
+        self.resistant_scope = resistant_scope or default_res_scope
+        self.time_scope = time_scope or default_time_range
+        self.time_sequence_number = time_sequence_number or default_time_scope
+        self.perturbation_rate = perturbation_rate or default_perturbation_rate
+        self.square_scope = square_scope or default_square_scope
 
-    def generate(self, layer_number=None, debug=False):
+    def generate(self, debug=False):
 
-        if layer_number is None:
-            layer_number = np.random.randint(default_layer_scope[0], default_layer_scope[1])
+        layer_number = self._generate_layer_number()
 
         depth = self._generate_depth(layer_number)
 
@@ -60,11 +61,8 @@ class Generator:
             'square': square,
         }
 
-    def add_perturbation(self, db_data):
-        perturbation_scope = self.perturbation_scope
-        if self.perturbation_scope is None:
-            perturbation_scope = default_perturbation_range
-
+    @staticmethod
+    def add_perturbation(db_data, perturbation_rate):
         # 增加扰动
         # B 数据 在其原始数据的 (-15 ~ 80) % 之间抖动
         # 扰动因子 k = 历史扰动数据的平方和开方分之一
@@ -81,28 +79,35 @@ class Generator:
         # 方案2： 在上一个时间增加扰动
         for index, value in enumerate(db_data):
             last_value = db_data[index-1] if index > 0 else db_data[index]
-            perturbation_rate = np.random.randint(
-                    perturbation_scope[0], perturbation_scope[1]
+            pr = np.random.randint(
+                    -perturbation_rate, + perturbation_rate
             ) / 100
 
-            res_data[index] = last_value * (1 + perturbation_rate)
+            res_data[index] = last_value * (1 + pr)
 
         return res_data
 
-    def _generate_res(self):
-        scope = default_res_scope if self.resistant_scope is None else self.resistant_scope
+    def _generate_layer_number(self):
+        # print(self.layer_number_scope)
+        layer_number_scope = self.layer_number_scope
+        layer_number = np.random.randint(layer_number_scope[0], layer_number_scope[1] + 1)
+
+        return layer_number
+
+    def _generate_res(self, layer_number):
+        scope = self.resistant_scope
 
         # 在 0 - 1 内 生成随机数
-        pre_res = np.random.random_sample(self.layer_number + 1)
+        pre_res = np.random.random_sample(layer_number + 1)
         # min-max 划入 scope 范围内
         res = scope[0] + pre_res * (scope[1] - scope[0])
 
         return res
 
-    def _generate_depth(self):
-        scope = default_res_scope if self.depth_scope is None else self.depth_scope
+    def _generate_depth(self, layer_number):
+        scope = self.layer_number_scope
 
-        pre_depth = np.random.random_sample(self.layer_number)
+        pre_depth = np.random.random_sample(layer_number)
 
         # min-max 划入 scope 范围内
         depths = scope[0] + pre_depth * (scope[1] - scope[0])
@@ -116,7 +121,7 @@ class Generator:
         return depths
 
     def _generate_square(self):
-        scope = default_square_scope if self.squared is None else self.square_scope
+        scope = self.square_scope
 
         square = np.random.randint(
             scope[0],
@@ -126,9 +131,9 @@ class Generator:
         return square
 
     def _generate_time(self):
-        scope = default_time_scope if self.time_sequence_number is None else self.time_sequence_number
+        scope = self.time_sequence_number
         generate_number = np.random.randint(scope[0], scope[1])
-        time_range = default_time_range if self.time_scope is None else self.time_scope
+        time_range = self.time_scope
 
         # 方案1: 等距取样
         # times = np.linspace(start=default_time_range[0], stop=default_time_range[1], num=time_sequence_number)
@@ -148,7 +153,7 @@ if __name__ == '__main__':
     data = generator.generate(debug=True)
 
     plt.plot(data['time'], data['origin_data'], label='origin')
-    plt.plot(data['time'], data['result_data'], label='result', alpha=0.6)
+    plt.plot(data['time'], data['result_data'], label='result', alpha=0.9, ls='--', lw=1)
 
     plt.legend()
     plt.show()
